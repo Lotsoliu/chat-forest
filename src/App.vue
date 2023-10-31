2f9053180341ea68a62b5475c34162206327fe9c
Lotsoliu <lx2733@outlook.com>
2023-10-31 20:43:57 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Scaner from './components/Scaner.vue';
+</script>
+
+<template>
+  <div>
+    <Scaner/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
